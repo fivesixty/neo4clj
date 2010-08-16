73cda5efe657d133240d1792fa978a39fec3bd13
c-spencer <cs560@cam.ac.uk>
2010-08-16 15:35:05 +0100

    Prepare structure for mixed java/clojure code.

--- project.clj
+++ project.clj
@@ -6,7 +6,11 @@
                  [org.neo4j/neo4j-index "1.1"]]
   ; :dev-dependencies [[swank-clojure "1.2.1"]]
   ; :dev-dependencies [[autodoc "0.7.1"]]
+  :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]]
   :repositories {"neo4j-public-repository" "http://m2.neo4j.org"}
+  
+  :source-path "src/clj"
+  :java-source-path "src/jvm"
   
   :warn-on-reflection true
   :aot [neo4clj.core])
---
+++ src/jvm/neo4clj/TestClass.java
@@ -0,0 +1,6 @@
+package neo4clj;
+
+public class TestClass {
+  public static int val = 4;
+  public TestClass() {}
+}
